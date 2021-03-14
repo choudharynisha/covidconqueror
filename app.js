31e2f388bffb728c9da1d04e38ce3b5752e3c757
wangjenny56 <20563230+wangjenny56@users.noreply.github.com>
2021-03-14 14:27:04 -0400

    updated with new queries for Dashboard

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 
 const uri = "mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19";
@@ -16,8 +19,11 @@ mongoose.connection.on('connected',() => {
 });
 
 const defaultSchema = new mongoose.Schema({});
-const globalAndUS = mongoose.model('globalAndUS', defaultSchema, 'global_and_us');
+const global_and_us = mongoose.model('global_and_us', defaultSchema, 'global_and_us');
 const global = mongoose.model('global', defaultSchema, 'global');
+const countries_summary = mongoose.model('countries_summary', defaultSchema, 'countries_summary');
+const metadata = mongoose.model('metadata', defaultSchema, 'metadata');
+const us_only = mongoose.model('us_only', defaultSchema, 'us_only');
 
 app.get("/", function(req, res){
     console.log("We are in the home page");
@@ -25,45 +31,39 @@ app.get("/", function(req, res){
   
 });
 
-app.get("/covidData", function(req, res){ 
-    console.log("We are getting data");
-    const data = [  
-        {label: "Jan. '13'", value: 53},
-        {label: "Feb. '13'", value: 165},
-        {label: "Mar. '13'", value: 269}, 
-        {label: "Apr. '13'", value: 344},
-        {label: "May  '13'", value: 376},
-        {label: "Jun. '13'", value: 410},
-        {label: "Jul. '13'", value: 421},   
-        {label: "Aug. '13'", value: 405},
-        {label: "Sep. '13'", value: 376},
-        {label: "Oct. '13'", value: 359},
-        {label: "Nov. '13'", value: 392},
-        {label: "Dec. '13'", value: 433},
-        {label: "Jan. '14'", value: 455},
-        {label: "Feb. '14'", value: 478}
-    ];
-    /*globalAndUS.find({ country: "France" }).sort({'date': -1}).limit(15).lean().exec(function(err, results) {
+/****************************************** CovidDashboard GlobalChart component  *******************************/
+//display global countries
+app.get("/allCountries", function(req, res){ 
+    console.log("We are getting countries data");
+    metadata.find().select('countries -_id').lean().exec(function(err, results) {
         if (err) {
             console.log('error', err);
             res.send(err);
         } else if (!results) {
             res.send(null);
         } else {
-            //console.log('results', results);
-            //res.send(data);
-            res.result;
+            res.send(results[0]);
         }
-    })*/
+    })
+});
 
-    global.find({ country: "US" }).sort({'date': -1}).limit(10).lean().exec(function(err, results) {
+
+//covid data for global countries
+app.post("/dataGlobalWithTimeRange", function(req, res){ 
+    console.log("We are getting data");
+    console.log(req.body);
+    countries_summary.find({ country: req.body.selectedCountry, date: {
+        $gte: new Date(req.body.startDate.slice(0,10)),
+        $lte: new Date(req.body.endDate.slice(0,10))
+    }
+    }).lean().exec(function(err, results) {
         if (err) {
             console.log('error', err);
             res.send(err);
         } else if (!results) {
             res.send(null);
         } else {
-            //console.log('results', results);
+            //res.send(results);
             var querydata = new Array();
             for (var i = 0; i < results.length; i++){
                 querydata.push({
@@ -72,56 +72,167 @@ app.get("/covidData", function(req, res){
                 });
             }
             res.send(querydata);
-            //res.send(data);
         }
     })
-  
 });
 
-app.get("/usDataForLastTenDays", function(req, res){ 
-    console.log("We are getting data");
 
-    //Get the last entry for france
-    global.find({ country: "US" }).sort({'date': -1}).limit(10).lean().exec(function(err, results) {
+/****************************************** CovidDashboard USCovidChart component  *******************************/
+//covid data for states
+app.post("/dataUSCovidWithTimeRange", function(req, res){ 
+    console.log("We are getting data");
+    console.log(req.body);
+    global_and_us.aggregate([
+        {$match:
+            {"country": "US",
+             "state" : req.body.selectedArea,
+             'date': { $gte: new Date(req.body.startDate.slice(0,10)),
+               $lte: new Date(req.body.endDate.slice(0,10)) } } },
+        {
+            "$group": {
+                "_id": "$date",
+                "value": { "$sum": "$confirmed_daily" }
+            }
+        }
+    ]).sort({'_id': 1}).exec(function(err, results) {
         if (err) {
             console.log('error', err);
             res.send(err);
         } else if (!results) {
             res.send(null);
         } else {
-            //console.log('results', results);
-            res.send(results);
-        }
-    })
-  
-});
-
-app.get("/franceData", function(req, res){ 
-    console.log("We are getting data");
-    global.find({ combined_name: "France" }).sort({'date': -1}).limit(10).lean().exec(function(err, results) {
-        if (err) {
-            console.log('error', err);
-            res.send(err);
-        } else if (!results) {
-            res.send(null);
-        } else {
-            //console.log('results', results);
             //res.send(results);
             var querydata = new Array();
             for (var i = 0; i < results.length; i++){
                 querydata.push({
-                    label: String(results[i].date).slice(3,10),
-                    confirmed: results[i].confirmed_daily,
-                    rcovered: results[i].recovered_daily
+                    label: String(results[i]._id).slice(3,10),
+                    value: results[i].value
                 });
             }
             res.send(querydata);
         }
     })
-    
-  
 });
-  
+
+//display all US states
+app.get("/dataUSStates", function(req, res){ 
+    console.log("We are getting data");
+    console.log(req.body);
+    metadata.find({}).select('states_us -_id').lean().exec(function(err, results) {
+        if (err) {
+            console.log('error', err);
+            res.send(err);
+        } else if (!results) {
+            res.send(null);
+        } else {
+            res.send(results[0]);
+        }
+    })
+});
+
+/****************************************** Recovered Dashboard GlobalRecoveredChart component  *******************************/
+//recovered data for global
+app.post("/dataGlobalRecovered", function(req, res){ 
+    console.log("We are getting data");
+    console.log(req.body);
+    countries_summary.find({ country: req.body.selectedCountry, date: {
+        $gte: new Date(req.body.startDate.slice(0,10)),
+        $lte: new Date(req.body.endDate.slice(0,10))
+    }
+    }).lean().exec(function(err, results) {
+        if (err) {
+            console.log('error', err);
+            res.send(err);
+        } else if (!results) {
+            res.send(null);
+        } else {
+            //res.send(results);
+            var querydata = new Array();
+            for (var i = 0; i < results.length; i++){
+                querydata.push({
+                    label: String(results[i].date).slice(3,10),
+                    value: results[i].recovered_daily
+                });
+            }
+            res.send(querydata);
+        }
+    })
+});
+
+/****************************************** Recovered Dashboard GlobalCovidAndRecoveredPieChart component  *******************************/
+//data for total covid and recovered cases for global country
+app.post("/dataGlobalCovidAndRecovered", function(req, res){ 
+    console.log("We are getting data");
+    console.log(req.body);
+    countries_summary.find({ country: req.body.selectedCountry}).sort({"date": -1}).limit(1).lean().exec(function(err, results) {
+        if (err) {
+            console.log('error', err);
+            res.send(err);
+        } else if (!results) {
+            res.send(null);
+        } else {
+            //res.send(results);
+            var querydata = new Array();
+            var popdata = new Array();
+            var casedata = new Array();
+            popdata.push({ 
+                name: "Population",
+                value: results[0].population
+            });
+            casedata.push({
+                name: "Covid Cases",
+                value: results[0].confirmed
+            });
+            casedata.push({
+                name: "Recovered Cases",
+                value: results[0].recovered
+            });
+            let healthyValue = 2*(results[0].confirmed + results[0].recovered);
+            casedata.push({
+                name: "Healthy",
+                value: healthyValue
+            });
+            
+            querydata.push(popdata);
+            querydata.push(casedata);
+            res.send(querydata);
+        }
+    })
+});
+
+
+/****************************************** Globe component  *******************************/
+//data for total covid cases for globe
+app.get("/dataGlobe", function(req, res){ 
+    console.log("We are getting data");
+    const yesterday = new Date(new Date);
+    yesterday.setDate(yesterday.getDate() - 1);
+    var value = yesterday.toISOString().split('T')[0];
+    global_and_us.find({date: new Date(value)}).lean().exec(function(err, results) {
+        if (err) {
+            console.log('error', err);
+            res.send(err);
+        } else if (!results) {
+            res.send(null);
+        } else {
+            //res.send(results);
+            var querydata = new Array();
+            for (var i = 0; i < results.length; i++){
+                if(results[i].hasOwnProperty("loc")){
+                    querydata.push({
+                        lat: results[i].loc.coordinates[0],
+                        lng: results[i].loc.coordinates[1],
+                        cases: results[i].confirmed,
+                        combined_name: results[i].combined_name
+                    });
+                }
+            }
+            res.send(querydata);
+        }
+    })
+});
+
+
 app.listen(5000, function(){
-    console.log("server started on 5000");
+    console.log("server started on 5000 filterCovid");
 });   
