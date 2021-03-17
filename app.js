5fe00aa5d32bc211e9446e8da110b99e7ddda2b3
Nisha Choudhary <choudharynisha024@gmail.com>
2021-03-17 11:28:52 -0400

    "Healthy → Not Affected", "Covid Cases → COVID Cases", Some Styling Consistency"

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 
 const uri = "mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19";
 
-//Set up default mongoose connection
+// Set up default mongoose connection
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -32,7 +32,7 @@ app.get("/", function(req, res){
 });
 
 /****************************************** CovidDashboard GlobalChart component  *******************************/
-//display global countries
+// display global countries
 app.get("/allCountries", function(req, res){ 
     console.log("We are getting countries data");
     metadata.find().select('countries -_id').lean().exec(function(err, results) {
@@ -47,8 +47,7 @@ app.get("/allCountries", function(req, res){
     })
 });
 
-
-//covid data for global countries
+// covid data for global countries
 app.post("/dataGlobalWithTimeRange", function(req, res){ 
     console.log("We are getting data");
     console.log(req.body);
@@ -76,7 +75,6 @@ app.post("/dataGlobalWithTimeRange", function(req, res){
     })
 });
 
-
 /****************************************** CovidDashboard USCovidChart component  *******************************/
 //covid data for states
 app.post("/dataUSCovidWithTimeRange", function(req, res){ 
@@ -98,23 +96,25 @@ app.post("/dataUSCovidWithTimeRange", function(req, res){
         if (err) {
             console.log('error', err);
             res.send(err);
-        } else if (!results) {
+        } else if(!results) {
             res.send(null);
         } else {
             //res.send(results);
             var querydata = new Array();
-            for (var i = 0; i < results.length; i++){
+
+            for(var i = 0; i < results.length; i++){
                 querydata.push({
                     label: String(results[i]._id).slice(3,10),
                     value: results[i].value
                 });
             }
+
             res.send(querydata);
         }
     })
 });
 
-//display all US states
+// display all US states
 app.get("/dataUSStates", function(req, res){ 
     console.log("We are getting data");
     console.log(req.body);
@@ -131,7 +131,7 @@ app.get("/dataUSStates", function(req, res){
 });
 
 /****************************************** Recovered Dashboard GlobalRecoveredChart component  *******************************/
-//recovered data for global
+// recovered data for global
 app.post("/dataGlobalRecovered", function(req, res){ 
     console.log("We are getting data");
     console.log(req.body);
@@ -160,7 +160,7 @@ app.post("/dataGlobalRecovered", function(req, res){
 });
 
 /****************************************** Recovered Dashboard GlobalCovidAndRecoveredPieChart component  *******************************/
-//data for total covid and recovered cases for global country
+// data for total covid and recovered cases for global country
 app.post("/dataGlobalCovidAndRecovered", function(req, res){ 
     console.log("We are getting data");
     console.log(req.body);
@@ -175,22 +175,26 @@ app.post("/dataGlobalCovidAndRecovered", function(req, res){
             var querydata = new Array();
             var popdata = new Array();
             var casedata = new Array();
+
             popdata.push({ 
                 name: "Population",
                 value: results[0].population
             });
+
             casedata.push({
-                name: "Covid Cases",
+                name: "COVID Cases",
                 value: results[0].confirmed
             });
+
             casedata.push({
                 name: "Recovered Cases",
                 value: results[0].recovered
             });
-            let healthyValue = 2*(results[0].confirmed + results[0].recovered);
+
+            let notAffected = (results[0].population - (results[0].confirmed + results[0].recovered));
             casedata.push({
-                name: "Healthy",
-                value: healthyValue
+                name: "Not affected",
+                value: notAffected
             });
             
             querydata.push(popdata);
@@ -200,9 +204,8 @@ app.post("/dataGlobalCovidAndRecovered", function(req, res){
     })
 });
 
-
 /****************************************** Globe component  *******************************/
-//data for total covid cases for globe
+// data for total covid cases for globe
 app.get("/dataGlobe", function(req, res){ 
     console.log("We are getting data");
     const yesterday = new Date(new Date);
@@ -232,7 +235,6 @@ app.get("/dataGlobe", function(req, res){
     })
 });
 
-
 app.listen(5000, function(){
     console.log("server started on 5000 filterCovid");
-});   
+});
