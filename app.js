aba2d002fcc94689733d039851d46b07c205968b
Nisha Choudhary <choudharynisha024@gmail.com>
2021-04-17 19:05:10 -0400

    Adding PORT and Heroku-Specific Code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -318,6 +320,17 @@ app.post("/dataGlobalAreaChart", function(req, res){
     })
 });
 
-app.listen(5000, function(){
-    console.log("server started on 5000 filterCovid");
+// Server static assets in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+  
+    // Serve all paths to point to the client folder's build folder index.html file
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+
+app.listen(PORT, function() {
+    console.log(`server started on ${PORT} filterCovid`);
 });
