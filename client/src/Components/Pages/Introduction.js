0d9614d8e5fcb31a495d61d0337b09f8e974cbfe
Nisha Choudhary <choudharynisha024@gmail.com>
2021-04-25 18:54:05 -0400

    Updated the Video Position

--- client/src/Components/Pages/Introduction.js
+++ client/src/Components/Pages/Introduction.js
@@ -1,18 +1,17 @@
 import '../../App.css';
 import Threebuttons from '../Navbar/Threebuttons'
-import FooterPage from '../Navbar/Footer'
 import '../Navbar/Textbox.css'
-
 
 function Introduction() {
     return (
         <div className = "App">
             <center>  
-            <div className="Textbox">
+            <div className = "Textbox">
             <br/>
              <br/>
               <h2 className = "heading">Understanding Viruses and COVID-19</h2>
               <p className = "text">Children all over the world have had to change how they interact with each other because of COVID-19. In this mission, you will learn more about COVID-19, how it spreads and some of the symptoms associated with COVID-19. To start off, let's watch the video below where Tim and Moby respond to a question from young children about COVID-19.</p> 
+              <center><iframe width = "560" height = "315" src = "https://www.youtube.com/embed/GoXxmzKdick?start=24&end=101" title = "YouTube video player" frameborder = "0"></iframe></center>
               <p className = "text">Like the kids who sent the letter to Tim and Moby, you have probably seen some very scary news about COVID-19 since the begining of 2020. At some point you probably wondered why things have changed at school. Why do you have to attend classes from home sometimes? Why do you have to wear a face mask when you are with other people? And why are most adults wearing face masks outside?</p>
               <p className = "text">COVID-19 is a disease that is caused by a virus called coronavirus. The name COVID-19 stands for Coronavirus Disease of 2019.</p>
               <p className = "text">Viruses are tiny particles that can invade living cells. There are millions of types of viruses, with different behaviors, shapes, and structures. Coronaviruses are one group, named for their crown-shaped outline. To survive, a virus needs a living cell where it can make more copies itself. So it invades an organism's body and turns its cells into virus factories. When the viruses invade the body, the make the body weak and sometimes result in sickness or diseases.</p>
@@ -26,7 +25,6 @@ function Introduction() {
                      <li>Inability to wake or stay awake</li>
                      <li>Pale, gray, or blue-colored skin, lips, or nail beds, depending on skin tone</li>
                 </ul>
-              <center><iframe width = "560" height = "315" src = "https://www.youtube.com/embed/GoXxmzKdick?start=24&end=101" title = "YouTube video player" frameborder = "0"></iframe></center>
               </div>
               <div className = "reflection">
               <h2>Answer on the questions below then click and hold on each card for correct answers.</h2>
